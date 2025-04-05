ab5683d023a1cce251fdb5913b8d96b5e24268ac
stk <igortimst@gmail.com>
2025-04-05 17:48:47 +0300

    fix

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,11 +1,12 @@
-import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { StateSchema } from './StateSchema';
 import { counterReducer } from 'entitis/Counter';
 import { userReducer } from 'entitis/User';
 import { createReducerManager } from './reducerManager';
 
-export function createReduxStore(initialState: StateSchema) {
+export function createReduxStore(initialState: StateSchema, asyncReducers?: ReducersMapObject<StateSchema>) {
     const rootReducer: ReducersMapObject<StateSchema> = {
+        ...asyncReducers,
         counter: counterReducer,
         user: userReducer,
     };
