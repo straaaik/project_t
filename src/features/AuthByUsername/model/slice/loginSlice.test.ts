92f8f4589f878934cc83575eb02990554ad7a25c
stk <igortimst@gmail.com>
2025-04-08 16:00:10 +0300

    fix test loginSlice

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -5,10 +5,10 @@ import { loginActions, loginReducer } from './loginSlice';
 describe('loginSlice', () => {
     test('test set username', () => {
         const state: DeepPartial<LoginSchema> = { username: '123' };
-        expect(loginReducer(state as LoginSchema, loginActions.setUsername('122323'))).toBe({ username: '122323' });
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('122323'))).toEqual({ username: '122323' });
     });
     test('test set password', () => {
         const state: DeepPartial<LoginSchema> = { password: '123' };
-        expect(loginReducer(state as LoginSchema, loginActions.setPassword('122323'))).toBe({ password: '122323' });
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('122323'))).toEqual({ password: '122323' });
     });
 });
